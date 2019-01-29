cb841dd65b6ad4af156be9ded5028501a302a136
Fractured2K <fractured2k@gmail.com>
2019-01-29 13:02:28 -0800

    Added navigation event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-// Your code goes here
+// Navigation
+const navHover = document.querySelectorAll('.nav-link');
+
+navHover.forEach(el => {
+    el.addEventListener('mouseover', (e) => {
+        e.currentTarget.style.color = '#17A2B8';
+    })
+})
+
+navHover.forEach(el => {
+    el.addEventListener('mouseout', (e) => {
+        e.currentTarget.style.color = 'black';
+    })
+})
