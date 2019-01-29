f66bc19046309f395cecd5828cb7f91c35505c1b
Fractured2K <fractured2k@gmail.com>
2019-01-29 15:50:55 -0800

    Added rng button handler and Form input focus/blur handler

--- js/index.js
+++ js/index.js
@@ -7,12 +7,26 @@ window.onload = () => {
 // Random title generator for heading
 window.onscroll = () => {
     const randomSelect = document.querySelector('h2');
-    const randomTitle = ['Lorem ipsum', 'Consectetur adipiscing elit', 'Sed accumsan ante turpis'];
+    const randomTitle = ['Lorem ipsum', 'Consectetur adipiscing elit', 'Sed accumsan ante turpis', ' Curabitur interdum enim', 'Nulla bibendum neque erat', 'Mauris elementum porta augue'];
 
     const rngTitle = Math.floor(Math.random() * randomTitle.length);
 
     randomSelect.textContent = randomTitle[rngTitle];
 }
+
+// Random button color
+window.addEventListener('resize', (e) => {
+    if (window.innerWidth < 1500) {
+        const colors = ['red', 'green', 'blue', 'orange'];
+        const rngColor = Math.floor(Math.random() * colors.length);
+        e.stopPropagation();
+
+        const btn = document.querySelectorAll('.btn');
+        btn.forEach(btn => {
+            btn.style.color = colors[rngColor];
+        })
+    }
+})
 
 // Team Fun Bus Easter Egg
 const funBus = document.querySelector('.logo-heading');
@@ -70,6 +84,20 @@ imageDrag.forEach(img => {
 let keys = [];
 window.addEventListener('keydown', e => {
     keys.push(e.key)
-    console.log(keys.toString())
     document.querySelector('.home header p').textContent = keys.join('');
+})
+
+
+const formInput = document.querySelectorAll('form input');
+
+formInput.forEach(input => {
+    input.addEventListener('focus', e => {
+        e.currentTarget.style.background = 'red';
+    })
+})
+
+formInput.forEach(input => {
+    input.addEventListener('blur', e => {
+        e.currentTarget.style.background = '';
+    })
 })
